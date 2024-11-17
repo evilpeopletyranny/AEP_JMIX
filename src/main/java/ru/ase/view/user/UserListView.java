8360f1f4f2d427f6fb64846b944e373d6a4a893f
vlad <evilpeopletyranny@yandex.ru>
2024-11-17 18:04:05 +0300

    init

---
+++ src/main/java/ru/ase/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package ru.ase.view.user;
+
+import ru.ase.entity.User;
+import ru.ase.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
